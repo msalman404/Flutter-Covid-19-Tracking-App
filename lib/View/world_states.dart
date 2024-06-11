4b04db1ff7b86d841a05013fcddefa9d4f1bad2d
Muhammad Salman <m.salman181920@gmail.com>
2024-06-11 14:51:35 +0500

    some changes

--- lib/View/world_states.dart
+++ lib/View/world_states.dart
@@ -36,93 +36,91 @@ class _WorldStatesScreenState extends State<WorldStatesScreen>
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.all(15),
-        child: Column(children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.1,
-          ),
-          FutureBuilder(
-              future: statesServices.fetchStatesApi(),
-              builder: (context, AsyncSnapshot<RecordModel> snapshot) {
-                if (!snapshot.hasData) {
-                  return Expanded(
-                      flex: 1,
-                      child: SpinKitFadingCircle(
-                        color: Colors.white,
-                        size: 100,
-                        controller: _controller,
-                      ));
-                } else {
-                  return Column(
-                    children: [
-                      PieChart(
-                        dataMap: {
-                          "Total":
-                              double.parse(snapshot.data!.cases.toString()),
-                          "Deaths":
-                              double.parse(snapshot.data!.deaths.toString()),
-                          "Recovered":
-                              double.parse(snapshot.data!.recovered.toString())
-                        },
-                        animationDuration: const Duration(milliseconds: 1200),
-                        chartType: ChartType.ring,
-                        legendOptions: const LegendOptions(
-                            legendPosition: LegendPosition.left),
-                        chartValuesOptions: const ChartValuesOptions(
-                            showChartValuesInPercentage: true),
-                        colorList: _colorList,
-                        chartRadius: 150.0,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: MediaQuery.of(context).size.height * .06),
-                        child: Card(
-                          child: Column(
-                            children: [
-                              ReusableRow(
-                                  title: 'Total Cases',
-                                  value: snapshot.data!.cases.toString()),
-                              ReusableRow(
-                                  title: 'Deaths',
-                                  value: snapshot.data!.deaths.toString()),
-                              ReusableRow(
-                                  title: 'Recovered',
-                                  value: snapshot.data!.recovered.toString()),
-                              ReusableRow(
-                                  title: 'Active Cases',
-                                  value: snapshot.data!.active.toString()),
-                              ReusableRow(
-                                  title: 'Critical',
-                                  value: snapshot.data!.critical.toString()),
-                              ReusableRow(
-                                  title: 'Today cases',
-                                  value: snapshot.data!.todayCases.toString()),
-                            ],
+        child: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            FutureBuilder(
+                future: statesServices.fetchStatesApi(),
+                builder: (context, AsyncSnapshot<RecordModel> snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else {
+                    return Column(
+                      children: [
+                        PieChart(
+                          dataMap: {
+                            "Total":
+                                double.parse(snapshot.data!.cases.toString()),
+                            "Deaths":
+                                double.parse(snapshot.data!.deaths.toString()),
+                            "Recovered": double.parse(
+                                snapshot.data!.recovered.toString())
+                          },
+                          animationDuration: const Duration(milliseconds: 1200),
+                          chartType: ChartType.ring,
+                          legendOptions: const LegendOptions(
+                              legendPosition: LegendPosition.left),
+                          chartValuesOptions: const ChartValuesOptions(
+                              showChartValuesInPercentage: true),
+                          colorList: _colorList,
+                          chartRadius: 150.0,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical:
+                                  MediaQuery.of(context).size.height * .06),
+                          child: Card(
+                            child: Column(
+                              children: [
+                                ReusableRow(
+                                    title: 'Total Cases',
+                                    value: snapshot.data!.cases.toString()),
+                                ReusableRow(
+                                    title: 'Deaths',
+                                    value: snapshot.data!.deaths.toString()),
+                                ReusableRow(
+                                    title: 'Recovered',
+                                    value: snapshot.data!.recovered.toString()),
+                                ReusableRow(
+                                    title: 'Active Cases',
+                                    value: snapshot.data!.active.toString()),
+                                ReusableRow(
+                                    title: 'Critical',
+                                    value: snapshot.data!.critical.toString()),
+                                ReusableRow(
+                                    title: 'Today cases',
+                                    value:
+                                        snapshot.data!.todayCases.toString()),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const CountriesListScreen()));
-                        },
-                        child: Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: const Color(0xff1aa260)),
-                          child: const Center(
-                            child: Text('Track Countries'),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const CountriesListScreen()));
+                          },
+                          child: Container(
+                            height: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: const Color(0xff1aa260)),
+                            child: const Center(
+                              child: Text('Track Countries'),
+                            ),
                           ),
-                        ),
-                      )
-                    ],
-                  );
-                }
-              }),
-        ]),
+                        )
+                      ],
+                    );
+                  }
+                }),
+          ]),
+        ),
       )),
     );
   }
